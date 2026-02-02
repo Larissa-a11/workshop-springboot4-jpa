798d44185aeb0ab4ff218623223f3eb9bf71153d
Larissa Almeida <larih.a.rodrigues@gmail.com>
2026-02-02 13:54:22 -0300

    Project created

---
+++ src/test/java/com/estudojpa/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudojpa.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
