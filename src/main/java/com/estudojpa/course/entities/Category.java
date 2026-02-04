468e2cd88c48e78cf4c85bdb91b32de7c228306a
Larissa Almeida <larih.a.rodrigues@gmail.com>
2026-02-04 14:33:17 -0300

    Product

--- src/main/java/com/estudojpa/course/entities/Category.java
+++ src/main/java/com/estudojpa/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.estudojpa.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +15,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    private Set<Product> products = new HashSet<>();
 
     public Category(){
 
@@ -38,6 +42,10 @@ public class Category implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    @Transient
+    public Set<Product> getProducts() {
+        return products;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -49,4 +57,5 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
